0b95eccb42e44db569bb26bc88401601097efac6
Adam Siwiec <acs0029@mail.wvu.edu>
2021-02-10 17:36:41 -0500

    created the skeleton from a tutorial - next push will have brightness/volume sound control for pc

--- voice.py
+++ voice.py
@@ -1,16 +1,45 @@
-import speech_recognition as evren
+import speech_recognition as s
 import pyaudio
+import pyttsx3
 
-listener = evren.Recognizer()
+listener = s.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.say('')
 
-try:
-    with evren.Microphone() as source:
-        print('listening...')
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
-        if 'evren' or "evrett" or "ev" in command:
-            print(command)
-except AttributeError or Exception as e:
-    print(f"Encountered an error.. ({e})")
-    pass
+def talk(text):
+    engine.say(text)
+    # engine.say('How can I help you?')
+    engine.runAndWait()
+
+def take_command():
+    try:
+        with s.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            # if 'alexa' in command:
+            #     # talk(command)
+    except AttributeError or Exception as e:
+        print(f"Encountered an error.. ({e})")
+        pass
+    return command
+
+def play_song(command):
+    song = command.replace('play', '')
+    song2 = command.rsplit('play', 2)
+    talk('playing' + song2[1])
+    print(song)
+    print(song2[1])
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        play_song(command)
+
+run_alexa()
+
